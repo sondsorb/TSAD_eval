8d788ab5eabc723502ed3f06dec800222b94bdda
Sondre Sørbø <sondresorbo@hotmail.no>
2023-01-11 10:54:41 +0100

    updated threshold figure

--- threshold_plt.py
+++ threshold_plt.py
@@ -2,10 +2,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-x = np.arange(60)
+x = np.arange(48)
 
-y = np.sin(0.7 + x / 15) + np.sin(x / 5 + 29) + 0.1 * np.sin(x) * (np.cos(np.sqrt(x) + 2)) + x / 40
+y = np.sin(0.7 + x / 12) + np.sin(x / 4 + 29) + 0.1 * np.sin(1.25*x) * (np.cos(np.sqrt(1.25*x) + 2)) + x / 32 + 0.12
 
+
+figsize=(3.4,2)
+plt.figure(figsize=figsize)
 
 plt.plot(x, y)
 
@@ -15,4 +18,6 @@ for t in [0.5, 1, 1.5, 2, 2.5]:
         if t < y[i]:
             plt.plot([x[i]], [t], ".r")
 
+plt.tight_layout()
+plt.savefig("thr2.pdf")
 plt.show()
